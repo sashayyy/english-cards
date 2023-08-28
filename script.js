534874a4ec12e8ef476f56b4fdef55b2c5cdf201
Aleksanya <allexandra.ya@gmail.com>
2023-08-28 21:48:47 +0300

    added browser history for cards using localStorage, fixed text-align in style.css

--- script.js
+++ script.js
@@ -2,13 +2,28 @@ let inputFindWord = document.getElementById('query');
 let buttonFind = document.getElementById('find');
 let cardsContainer = document.getElementsByClassName('cards-container')[0];
 
+let cards = JSON.parse(localStorage.getItem('cards'));
+
+if (localStorage.getItem('cards') !== null) {
+    cards.forEach(element => {
+        let card = document.createElement('div');
+        card.className = 'card';
+        card.insertAdjacentHTML('afterbegin', `<figure><img src=${element.imgUrl} width="210px" height="210px"></figure>`);
+        card.insertAdjacentHTML('beforeend', `<p class="answer-en">${element.enText}</p>`);
+        card.insertAdjacentHTML('beforeend', `<p class="answer-ru">${element.ruText}</p>`);
+    
+        cardsContainer.append(card);
+    });
+}
+
 buttonFind.addEventListener("click", addCard);
-document.addEventListener('keydown', async (event) => {
+document.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
         event.preventDefault();
         addCard();
     }
 });
+
 
 async function imageRequest(query) {
     const requestURL = `https://api.unsplash.com/search/photos?client_id=4DfpRxdNmDx1AKmWWg-oZsa7Hn2rMJFAeU3hGJzjyqk&page=1&query=${query}&orientation=squarish`;
@@ -32,6 +47,10 @@ async function translateRequest(textToTranslate) {
     return jsonObj['data']['translations'][0]['translatedText'];
 }
 
+function updateLocalStorage(data) {
+    localStorage.setItem('cards', JSON.stringify(data));
+}
+
 async function addCard() {
     let enText = document.getElementById('query').value;
     let ruText = await translateRequest(enText);
@@ -42,9 +61,8 @@ async function addCard() {
     card.insertAdjacentHTML('afterbegin', `<figure><img src=${imgUrl} width="210px" height="210px"></figure>`);
     card.insertAdjacentHTML('beforeend', `<p class="answer-en">${enText}</p>`);
     card.insertAdjacentHTML('beforeend', `<p class="answer-ru">${ruText}</p>`);
-
     cardsContainer.prepend(card);
+
+    cards.unshift({ imgUrl: imgUrl, ruText: ruText, enText: enText });
+    updateLocalStorage(cards);
 }
-
-
-
